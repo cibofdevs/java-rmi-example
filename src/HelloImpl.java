bf868d67abc5e9e339b51fb54e70937c501918b1
Ahmad Wijaya <cibofdev@gmail.com>
2024-06-20 23:18:43 +0700

    added example code

---
+++ src/HelloImpl.java
@@ -0,0 +1,13 @@
+import java.rmi.server.UnicastRemoteObject;
+import java.rmi.RemoteException;
+
+public class HelloImpl extends UnicastRemoteObject implements Hello {
+    protected HelloImpl() throws RemoteException {
+        super();
+    }
+
+    @Override
+    public String sayHello() throws RemoteException {
+        return "Hello, world!";
+    }
+}
